bc996bf89b56c5d7b13f736ddc13b8b2d29e177e
Sohag558 <154774449+Sohag558@users.noreply.github.com>
2024-01-15 23:10:12 +0600

    nav section added

---
+++ style.js
@@ -0,0 +1,15 @@
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.getElementById('menuIcon');
+    const menu = document.querySelector('.menu');
+
+    menuIcon.addEventListener('click', function () {
+      menu.classList.toggle('show');
+    });
+
+    document.addEventListener('click', function (event) {
+      if (!menu.contains(event.target) && event.target !== menuIcon) {
+        menu.classList.remove('show');
+      }
+    });
+  });
